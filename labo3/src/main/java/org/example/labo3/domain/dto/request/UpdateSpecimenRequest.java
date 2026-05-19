9c1cf82bef80220c140538528b2d8bdefb067ecb
HeberC2003 <00054621@uca.edu.sv>
2026-05-18 21:07:00 -0600

    feat: add DTOs for Specimen

---
+++ labo3/src/main/java/org/example/labo3/domain/dto/request/UpdateSpecimenRequest.java
@@ -0,0 +1,14 @@
+package org.example.labo3.domain.dto.request;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateSpecimenRequest {
+    private String name;
+    private String region;
+    private Integer dangerLevel;
+    private Boolean isFriendly;
+}
