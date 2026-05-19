220a246705197163a8435384bbf543e49f639f77
HeberC2003 <00054621@uca.edu.sv>
2026-05-18 21:22:36 -0600

    feat: add SpecimenRepository

---
+++ labo3/src/main/java/org/example/labo3/repositories/SpecimenRepository.java
@@ -0,0 +1,11 @@
+package org.example.labo3.repositories;
+
+import org.example.labo3.domain.entities.Specimen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SpecimenRepository extends JpaRepository<Specimen, UUID> {
+}
